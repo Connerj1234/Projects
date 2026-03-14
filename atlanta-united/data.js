bec9f45766e1bc8444e06b6b0eea5d796f58453b
Conner Jamison <mailtoclyjams@gmail.com>
2026-03-14 08:15:15 +0000

    chore(data): automated daily refresh (2026-03-14)

--- atlanta-united/data.js
+++ atlanta-united/data.js
@@ -291,7 +291,7 @@ window.ATL_DATA = {
       "id": "mls-2026-MLS-OBJ-0003T9",
       "name": "Miguel Almirón",
       "number": "10",
-      "position": "M",
+      "position": "F",
       "appearances": 3,
       "starts": 0,
       "goals": 0,
@@ -8485,7 +8485,7 @@ window.ATL_DATA = {
       "conference": "Eastern Conference",
       "isAtlanta": true
     },
-    "generatedAt": "2026-03-13T08:15:03.043Z"
+    "generatedAt": "2026-03-14T08:15:03.619Z"
   },
   "seasonHistory": [
     {
