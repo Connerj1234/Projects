44b280c94f7d25c854590bb4bde30e54fcaaefa2
Conner Jamison <mailtoclyjams@gmail.com>
2026-03-29 08:15:15 +0000

    chore(data): automated daily refresh (2026-03-29)

--- atlanta-united/data.js
+++ atlanta-united/data.js
@@ -8501,7 +8501,7 @@ window.ATL_DATA = {
       "conference": "Eastern Conference",
       "isAtlanta": true
     },
-    "generatedAt": "2026-03-28T08:15:03.545Z"
+    "generatedAt": "2026-03-29T08:15:03.163Z"
   },
   "seasonHistory": [
     {
